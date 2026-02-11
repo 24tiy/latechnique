90f21dbd9adb3180b29b6af7dfc31a50007ed4b6
Alexander Fokin <77535903+24tiy@users.noreply.github.com>
2026-02-11 09:05:53 +0100

    Update index.ts

--- components/design-system/index.ts
+++ components/design-system/index.ts
@@ -4,3 +4,4 @@ export { Card } from './Card';
 export { AnimatedBorder } from './AnimatedBorder';
 export { OrbsBackground } from './OrbsBackground';
 export { Header } from './Header';
+export { ScrollReveal } from './ScrollReveal';
