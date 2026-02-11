d179da74aa52183fb47556a927d194dd53ed7f26
Alexander Fokin <77535903+24tiy@users.noreply.github.com>
2026-02-11 09:05:29 +0100

    Update index.ts

--- components/landing/index.ts
+++ components/landing/index.ts
@@ -4,3 +4,5 @@ export { Features } from './Features';
 export { HowItWorks } from './HowItWorks';
 export { Pricing } from './Pricing';
 export { Footer } from './Footer';
+export { ScrollClouds } from './ScrollClouds';
+export { GrassFooter } from './GrassFooter';
