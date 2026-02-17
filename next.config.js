4ab4895b792c1734a54a9dbcd0b71824250f6351
Alexander Fokin <77535903+24tiy@users.noreply.github.com>
2026-02-17 13:04:58 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-  // Для GitHub Pages
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/latechnique' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/latechnique/' : '',
-  
-  images: {
-    unoptimized: true, // GitHub Pages не поддерживает Next.js Image Optimization
-  },
-  
-  // Отключаем trailing slash для корректной работы на GitHub Pages
+  images: { unoptimized: true },
   trailingSlash: true,
+  transpilePackages: ['three', 'gsap'],
 };
 
 module.exports = nextConfig;
